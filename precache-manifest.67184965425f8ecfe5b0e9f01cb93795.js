0c48a8a19f889f13c989be9522ce638082d91855
Niels Kootstra <niels.kootstra@gmail.com>
2020-06-02 14:57:42 +0200

    Updates

---
+++ precache-manifest.67184965425f8ecfe5b0e9f01cb93795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b02b195dd6819fcf0d671990c9fa96c",
+    "url": "/fut-tracker/index.html"
+  },
+  {
+    "revision": "eef87d5f6190ccc689d1",
+    "url": "/fut-tracker/static/css/main.a7c5557f.chunk.css"
+  },
+  {
+    "revision": "7321d1bd732285e50f40",
+    "url": "/fut-tracker/static/js/2.8448244b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fut-tracker/static/js/2.8448244b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef87d5f6190ccc689d1",
+    "url": "/fut-tracker/static/js/main.276646af.chunk.js"
+  },
+  {
+    "revision": "484bbc2ed53382d49f65",
+    "url": "/fut-tracker/static/js/runtime-main.abe7216e.js"
+  }
+]);
